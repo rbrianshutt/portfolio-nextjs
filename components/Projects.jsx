f3d7c3efda6e30a11bcaf3b701598d7a5cb2204f
rbrianshutt <rbrianshutt@gmai.com>
2022-11-02 14:08:39 -0500

    first commit

---
+++ components/Projects.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import propertyImg from '../public/projects/property.jpg';
+import cryptoImg from '../public/projects/crypto.jpg';
+import netflixImg from '../public/projects/netflix.jpg';
+import twitchImg from '../public/projects/twitch.jpg';
+import ProjectItem from '/components/ProjectItem';
+
+function Projects() {
+	return (
+		<div id='projects' className='w-full'>
+			<div className='max-w-[1240px] mx-auto px-2 py-16'>
+				<p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
+				<h2 className='py-4'>What I've Built</h2>
+				<div className='grid md:grid-cols-2 gap-8'>
+					<ProjectItem
+						title='Property Finder'
+						backgroundImg={propertyImg}
+						projectUrl='/property'
+					/>
+					<ProjectItem
+						title='Crypto App'
+						backgroundImg={cryptoImg}
+						projectUrl='/crypto'
+					/>
+					<ProjectItem
+						title='Netflix App'
+						backgroundImg={netflixImg}
+						projectUrl='/netflix'
+					/>
+					<ProjectItem title='Twitch UI' backgroundImg={twitchImg} projectUrl='/twitch' />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Projects;
